4b44aef8b3620f23e37d4a8b791e941bb93f7d83
Sato <sato@debian-BULLSEYE-live-builder-AMD64>
2023-04-12 13:29:34 +0300

    un commented an importent line

--- rc.lua
+++ rc.lua
@@ -580,7 +580,7 @@ client.connect_signal("request::titlebars", function(c)
 	    --awful.titlebar.widget.stickybutton   (c),
             --awful.titlebar.widget.ontopbutton    (c),
             --awful.titlebar.widget.closebutton    (c),
-            --layout = wibox.layout.fixed.horizontal()
+            layout = wibox.layout.fixed.horizontal()
         },
         layout = wibox.layout.align.horizontal
     }
